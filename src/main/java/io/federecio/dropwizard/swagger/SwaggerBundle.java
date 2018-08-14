2a5f24e6c331e4908710941be4fc8268e0e9e13c
Jason Fry <jasonfry89@gmail.com>
2018-08-14 14:25:49 -0700

    Sending

--- src/main/java/io/federecio/dropwizard/swagger/SwaggerBundle.java
+++ src/main/java/io/federecio/dropwizard/swagger/SwaggerBundle.java
@@ -1,12 +1,9 @@
 /**
  * Copyright (C) 2014 Federico Recio
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,21 +17,18 @@ import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Configuration;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.assets.AssetsBundle;
-import io.dropwizard.jersey.DropwizardResourceConfig;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
-import io.swagger.config.ScannerFactory;
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
 import io.swagger.models.Swagger;
-import io.swagger.models.auth.ApiKeyAuthDefinition;
-import io.swagger.models.auth.In;
-import io.swagger.models.auth.OAuth2Definition;
-import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.process.Inflector;
 import org.glassfish.jersey.server.model.Resource;
 
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.MediaType;
 import java.util.Map;
 
 /**
@@ -68,11 +62,16 @@ public abstract class SwaggerBundle<T extends Configuration> implements Configur
         new AssetsBundle(Constants.SWAGGER_RESOURCES_PATH, configurationHelper.getSwaggerUriPath(), null, Constants.SWAGGER_ASSETS_NAME).run(environment);
 
         // Register the resource that returns the swagger HTML
-//        String urlPattern = configurationHelper.getUrlPattern();
-//        Resource swaggerHTMLResource = Resource
-//                .builder(SwaggerResource.class)
-//                .build();
-//        environment.jersey().register(swaggerHTMLResource);
+        String urlPattern = configurationHelper.getUrlPattern();
+        Resource.Builder resourceBuilder = Resource
+                .builder()
+                .path(urlPattern);
+        resourceBuilder
+                .addMethod("GET")
+                .produces(MediaType.TEXT_HTML)
+                .handledBy((Inflector<ContainerRequestContext, SwaggerView>) containerRequestContext -> new SwaggerView(urlPattern));
+        Resource resource = resourceBuilder.build();
+        environment.jersey().getResourceConfig().registerResources(resource);
 //        environment.jersey().register(new SwaggerResource(urlPattern));
 
         BeanConfig beanConfig = setUpSwagger(swaggerBundleConfiguration, configurationHelper.getUrlPattern());
@@ -80,7 +79,7 @@ public abstract class SwaggerBundle<T extends Configuration> implements Configur
         // Register the resource that returns swagger.json
         Resource swaggerJSONResource = Resource
                 .builder(ApiListingResource.class)
-                .path("/swagger222.{type:json|yaml}")
+                .path(urlPattern + ".{type:json|yaml}")
                 .build();
         environment.jersey().getResourceConfig().registerResources(swaggerJSONResource);
 //        environment.jersey().register(new ApiListingResource());
