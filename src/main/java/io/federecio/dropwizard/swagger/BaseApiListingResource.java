0644e3965ad84ed8540443171b35045b58b3fb67
sholab <babalolashola@rocketmail.com>
2018-08-17 15:41:34 -0700

    create new instance of SwaggerContextService

--- src/main/java/io/federecio/dropwizard/swagger/BaseApiListingResource.java
+++ src/main/java/io/federecio/dropwizard/swagger/BaseApiListingResource.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2014 Federico Recio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.federecio.dropwizard.swagger;
 
 import io.swagger.config.FilterFactory;
@@ -43,9 +58,7 @@ public class BaseApiListingResource extends io.swagger.jaxrs.listing.BaseApiList
             UriInfo uriInfo) {
         String path = getPath(uriInfo);
 
-        SwaggerContextService ctxService = (SwaggerContextService) new SwaggerContextService()
-                .withServletConfig(sc)
-                .withBasePath(uriInfo.getBaseUri().getPath());
+        SwaggerContextService ctxService = new SwaggerContextService(sc,  uriInfo.getBaseUri().getPath());
 
         Swagger swagger = ctxService.getSwagger(path);
 
--- src/main/java/io/federecio/dropwizard/swagger/SwaggerContextService.java
+++ src/main/java/io/federecio/dropwizard/swagger/SwaggerContextService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2014 Federico Recio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.federecio.dropwizard.swagger;
 
 import io.swagger.jaxrs.config.SwaggerConfigLocator;
@@ -16,6 +31,11 @@ public class SwaggerContextService extends io.swagger.jaxrs.config.SwaggerContex
     private String contextId;
     private String basePath;
     private ServletConfig sc;
+
+    public SwaggerContextService(ServletConfig sc, String basePath) {
+        this.basePath = normalizeBasePath(basePath);
+        this.sc = sc;
+    }
 
     public Swagger getSwagger(String uriPath) {
         Swagger swagger = null;
@@ -47,5 +67,27 @@ public class SwaggerContextService extends io.swagger.jaxrs.config.SwaggerContex
             }
         }
         return SwaggerConfigLocator.getInstance().getSwagger(configIdKey);
+    }
+
+    /**
+     * Normalize base path to the canonical form by adding trailing and leading slashes
+     * @param basePath base path to normalize
+     * @return normalized base path
+     */
+    private static String normalizeBasePath(final String basePath) {
+        if (basePath == null) {
+            return basePath;
+        }
+
+        String normalizedBasePath = basePath.trim();
+        if (!normalizedBasePath.startsWith("/")) {
+            normalizedBasePath = "/" + normalizedBasePath;
+        }
+
+        if (!normalizedBasePath.endsWith("/")) {
+            normalizedBasePath = normalizedBasePath + "/";
+        }
+
+        return normalizedBasePath;
     }
 }
